be2e626edab5a057caaf2a74d77e9b6f86c1593f
Anthoni Caldwell <anthoni.caldwell@gmail.com>
2020-05-15 13:52:47 -0500

    Initial Code Commit.
    Initial Code Commit.

---
+++ StageAll.cs
@@ -0,0 +1,62 @@
+﻿using KSP.UI.Screens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace StageAll
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class StageAll : MonoBehaviour
+    {
+        public void OnGUI()
+        { 
+            if ( GUI.Button(new Rect(32,64,128,32), "Stage All") )
+            {
+                foreach (Vessel v in FlightGlobals.Vessels)
+                {
+                    //v.ActionGroups.ToggleGroup(KSPActionGroup.Stage);
+                    v.ResumeStaging();
+                    FlightGlobals.SetActiveVessel(v);
+                    StageManager.ActivateNextStage();
+
+                    foreach (Part p in v.parts)
+                    {
+                        if ( p.stageOffset == v.currentStage)
+                        {
+                            List<ModuleEngines> engines;
+                            if (p.isEngine(out engines))
+                            {
+                                foreach (ModuleEngines e in engines)
+                                {
+                                    e.throttleMin = 100.0f;
+                                    e.currentThrottle = 100.0f;
+                                    e.UpdateThrottle();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (GUI.Button(new Rect(32, 96, 128, 32), "Toggle SAS All"))
+            {
+                foreach ( Vessel v in FlightGlobals.Vessels)
+                {
+                    v.ActionGroups.ToggleGroup(KSPActionGroup.SAS);
+                }
+            }
+
+            if (GUI.Button(new Rect(32, 128, 128, 32), "Toggle RCS All"))
+            {
+                foreach (Vessel v in FlightGlobals.Vessels)
+                {
+                    v.ActionGroups.ToggleGroup(KSPActionGroup.RCS);
+                }
+            }
+        }
+    }
+}
